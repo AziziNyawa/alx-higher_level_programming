0113f73d7c433e4a7ad27dc94839ae614fe8faa5
AziziNyawa <nyawaazizi@gmail.com>
2023-06-05 18:51:39 +0000

    fixing errors

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that cobines remarkable power with very clear syntax"
-str = str[39:-62] + str[105:-16] + str[0:-122]
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:67] +  str[-23:-17]  +  str[0:6]
 print(str)
