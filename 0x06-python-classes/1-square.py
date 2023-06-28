69f8fefe37d3b4eee6930db58111523c8ecbc23e
AziziNyawa <nyawaazizi@gmail.com>
2023-06-28 09:36:47 +0000

    xhange

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,8 +7,8 @@ class Square:
     """Defines a Square Class"""
 
     def __init__(self,size):
-        """initializing the class using init
-        Arguments for the class is size rep of the class square defined
+        """Initializing the class using init
+        Args: size - for the class is size rep of the class square defined
         """
 
         self.__size = size
