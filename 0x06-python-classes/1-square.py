b02f68d9012aaf0a6d5c9883be716b1823e0563e
AziziNyawa <nyawaazizi@gmail.com>
2023-06-28 09:33:28 +0000

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,14 @@
 #!/usr/python3
-#1-square.py done by Azizi Nyawa
+# 1-square.py done by Azizi Nyawa
 """Defines a Square Class"""
+
 
 class Square:
     """Defines a Square Class"""
 
     def __init__(self,size):
         """initializing the class using init
-        Arguments for the class is size rep of the class
-        square defined"""
+        Arguments for the class is size rep of the class square defined
+        """
 
         self.__size = size
